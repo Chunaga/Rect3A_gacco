b2e725f90eea6933f1a9ab2e6fe73aedb41bbbe2
Natsu3 <shirayuki1005@yahoo.co.jp>
2019-03-19 10:15:47 +0900

    Added the method for task.

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args)  {
         Rect3A r1 = new Rect3A();
         r1.SetData(10,20,30,40);
-        System.out.println("長方形の位置"+ r1.x + "," + r1.y + "幅" + r1.width + "高さ" + r1.height);
+	r1.printInfo();
+	System.out.println("面積は" + r1.calcArea());
+	System.out.println("-------------------------");
+	System.out.println("x方向に30, y方向に50移動する");
+	r1.translate(30, 50);
+	r1.printInfo();
+
     }
 }
--- Rect3A.java
+++ Rect3A.java
@@ -10,4 +10,19 @@ class Rect3A{
         width = w;
         height = h;
     }
+    
+    void printInfo(){
+    	System.out.println("長方形の位置"+ x + "," + y + "幅" + width + "高さ" + height);
+    }
+    
+    void translate(int dx, int dy){
+    	x = x + dx; 
+	y = y + dy;
+    }
+    
+    double calcArea() {
+    	return width * height;
+    }
+    
+    
 }
